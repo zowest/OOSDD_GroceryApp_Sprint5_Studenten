571a0d3726ad2137769a745aecd8180142062ad3
Zoe <s1200037@student.windesheim.nl>
2025-10-06 19:30:18 +0200

    Add Price property to Product and update UI and logic

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -7,12 +7,15 @@ namespace Grocery.Core.Models
         [ObservableProperty]
         public int stock;
         public DateOnly ShelfLife { get; set; }
-        public Product(int id, string name, int stock) : this(id, name, stock, default) { }
 
-        public Product(int id, string name, int stock, DateOnly shelfLife) : base(id, name) 
+        public double Price { get; set; }
+        public Product(int id, string name, int stock, double price) : this(id, name, stock, default, price) { }
+
+        public Product(int id, string name, int stock, DateOnly shelfLife, double price) : base(id, name)
         {
             Stock = stock;
             ShelfLife = shelfLife;
+            Price = price;
         }
         public override string? ToString()
         {
